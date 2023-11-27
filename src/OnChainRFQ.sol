f8474256cfbbfe547c5764fde64ae155392baa53
Eric Zhong <eric.zhong@uniswap.org>
2023-11-26 20:24:58 -0500

    Add RFQ portion

--- src/OnChainRFQ.sol
+++ src/OnChainRFQ.sol
@@ -1,55 +1,79 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
 
-struct SignedOrder {
-    bytes order;
-    bytes sig;
-}
-
-struct AuctionOrder {
-    bytes32 orderHash;
-    SignedOrder signedOrder;
-    uint256 auctionEnd;
-    uint256 auctionStart;
-    uint256 revealDeadline;
-    uint256 exclusivityPeriodEnd;
-}
+import {IReactor} from "UniswapX/src/interfaces/IReactor.sol";
+import {SignedOrder} from "UniswapX/src/base/ReactorStructs.sol";
+import {QuoteRequest, QuoteRequestLib} from "./lib/QuoteRequestLib.sol";
 
 contract OnChainRFQ {
-    // Mapping from orderHash to mapping from quoter to commitment
-    mapping(bytes32 => mapping (address => bytes32)) public commitments;
+    using QuoteRequestLib for QuoteRequest;
+
+    // Mapping of request hash to mapping of quoter to commitment
+    mapping(bytes32 => mapping(address => bytes32)) public commitments;
     mapping(bytes32 => uint256) public winningQuote;
     mapping(bytes32 => address) public winningQuoter;
 
-    error ExclusivityPeriodNotOver;
+    error InvalidCommitment();
+    error ExclusivityPeriodNotOver();
 
-    function createCommitment(AuctionOrder memory order, uint256 quote) public view returns (bytes32) {
+    event CommitmentReceived(bytes32 indexed commitment, uint256 timestamp);
+    event CommitmentRevealed(bytes32 indexed commitment, uint256 value, uint256 timestamp);
+    event AuctionStarted(bytes32 indexed requestHash, QuoteRequest request);
+
+    IReactor public reactor;
+
+    constructor(IReactor _reactor) {
+        reactor = _reactor;
+    }
+
+    /// @notice Start an auction for an order
+    /// fillers must listen for the AuctionStarted event and then submit their quotes
+    function startAuction(QuoteRequest memory request) public {
+        require(block.timestamp < request.auctionStart);
+        require(block.timestamp < request.auctionEnd);
+        require(request.auctionEnd > request.auctionStart);
+        require(request.revealDeadline > request.auctionEnd);
+        require(request.exclusivityPeriodEnd > request.revealDeadline);
+        require(request.exclusivityPeriodEnd > request.auctionEnd);
+        require(request.exclusivityPeriodEnd > request.auctionStart);
+        require(request.exclusivityPeriodEnd > block.timestamp);
+
+        emit AuctionStarted(request.hash(), request);
+    }
+
+    function generateCommitment(QuoteRequest memory order, uint256 quote) public view returns (bytes32) {
         return keccak256(abi.encode(msg.sender, order, quote));
     }
 
-    function commit(AuctionOrder memory order, bytes32 _commitment) public {
-        require(block.timestamp < order.auctionEnd);
-        commitments[order.orderHash][msg.sender] = _commitment;
+    function commitQuote(QuoteRequest memory request, bytes32 _commitment) public {
+        require(block.timestamp < request.auctionEnd);
+        commitments[request.hash()][msg.sender] = _commitment;
+        emit CommitmentReceived(_commitment, block.timestamp);
     }
 
     /// @notice Reveal your quote for the auction
     /// this will leak information to other participants, who can choose to not reveal if they lose.
-    function reveal(AuctionOrder memory order, uint256 quote) public {
-        require(block.timestamp > order.auctionEnd);
-        require(block.timestamp <= order.revealDeadline);
-        require(createCommitment(order, quote) == commitments[order.orderHash][msg.sender]);
-        if(quote > winningQuote[order.orderHash]) {
-            winningQuote[order.orderHash] = quote;
-            winningQuoter[order.orderHash] = msg.sender;
+    function revealQuote(QuoteRequest memory request, uint256 quote) public {
+        require(block.timestamp > request.auctionEnd);
+        require(block.timestamp <= request.revealDeadline);
+
+        bytes32 hash = request.hash();
+        bytes32 commitment = generateCommitment(request, quote);
+        if(commitment != commitments[hash][msg.sender]) {
+            revert InvalidCommitment();
         }
+        if (quote > winningQuote[hash]) {
+            winningQuote[hash] = quote;
+            winningQuoter[hash] = msg.sender;
+        }
+        emit CommitmentRevealed(commitment, quote, block.timestamp);
     }
 
-    function settle(AuctionOrder memory order) public {
-        require(block.timestamp > order.revealDeadline, "Auction not over yet");
-        if(block.timestamp <= order.exclusivityPeriodEnd && winningQuoter[order.orderHash] != msg.sender) {
-            revert ExclusivityPeriodNotOver();
-        }
-
-
+    /// @notice Get the winning quote for the auction
+    /// called offchain to create the eventual order
+    function getWinningQuote(QuoteRequest memory request) public view returns (address, uint256) {
+        require(block.timestamp > request.revealDeadline, "Auction not over yet");
+        bytes32 hash = request.hash();
+        return (winningQuoter[hash], winningQuote[hash]);
     }
 }
---
+++ src/lib/QuoteRequestLib.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import {InputToken, OutputToken} from "UniswapX/src/base/ReactorStructs.sol";
+
+struct QuoteRequest {
+    InputToken input;
+    uint256 auctionStart;
+    uint256 auctionEnd;
+    uint256 revealDeadline;
+    uint256 exclusivityPeriodEnd;
+}
+
+library QuoteRequestLib {
+    bytes internal constant QUOTE_REQUEST_TYPE = abi.encodePacked(
+        "QuoteRequest(",
+        "address inputToken,",
+        "uint256 inputStartAmount,",
+        "uint256 inputEndAmount,",
+        "uint256 auctionStart,",
+        "uint256 auctionEnd,",
+        "uint256 revealDeadline,",
+        "uint256 exclusivityPeriodEnd)"
+    );
+
+    bytes32 internal constant QUOTE_REQUEST_TYPE_HASH = keccak256(QUOTE_REQUEST_TYPE);
+
+    /// @notice hash the given quote request
+    /// @param request the request to hash
+    /// @return the eip-712 request hash
+    function hash(QuoteRequest memory request) internal pure returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                QUOTE_REQUEST_TYPE_HASH,
+                request.input.token,
+                request.input.amount,
+                request.input.maxAmount,
+                request.auctionStart,
+                request.auctionEnd,
+                request.revealDeadline,
+                request.exclusivityPeriodEnd
+            )
+        );
+    }
+}
